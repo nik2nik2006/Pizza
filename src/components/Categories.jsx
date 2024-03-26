42f964dbdfb97f53c509acaf008f5295dbb9d4ce
nik2nik2006 <belnv@yandex.ru>
2024-03-26 19:30:08 +0300

    добавил юзстейт в пиццаблок

---
+++ src/components/Categories.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Categories() {
+    const [activeIndex, setActiveIndex] = React.useState(0)
+    const categories = [
+        'Все',
+        'Мясные',
+        'Вегетарианская',
+        'Гриль',
+        'Острые',
+        'Закрытые'
+    ]
+    const onClickCategory = (index) => {
+        setActiveIndex(index)
+    }
+    return (
+        <div className="categories">
+            <ul>
+                {categories.map((value, index) => {
+                    return <li onClick={() => onClickCategory(index)}
+                               className={activeIndex === index ? "active" : ''}>{value}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default Categories
