e9e05aa192afa00db7f5b5667be912bff3a62614
nik2nik2006 <belnv@yandex.ru>
2024-04-05 21:25:01 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from "react";
 import './scss/app.scss';
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
-import pizzas from './assets/pizzas.json'
+// const pizzas = [];
 
 function App() {
+    const [items, setItems] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('https://6603e6a62393662c31d00976.mockapi.io/items')
+            .then(res => res.json())
+            .then(arr => {
+                setItems(arr);
+            });
+    }, [])
+
+
     return (
         <div className="wrapper">
             <Header/>
@@ -18,7 +30,7 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((obj) => {
+                        {items.map((obj) => {
                                 return <PizzaBlock key={obj.id} {...obj}
                                     // title={obj.title}
                                     // price={obj.price}
