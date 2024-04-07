6bf4162817804534aecf7df38dede1672d48f4fc
nik2nik2006 <belnv@yandex.ru>
2024-04-07 10:12:28 +0300

    step 8. Создал страницу карзины

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,10 +15,11 @@ const Home = () => {
             .then((arr) => {
                     setItems(arr);
                     setIsLoading(false);
-                }
-            )}, []);
+                });
+        window.scrollTo(0, 0);
+    }, []);
     return (
-        <>
+        <div className="container">
             <div className="content__top">
                 <Categories/>
                 <Sort/>
@@ -31,7 +32,7 @@ const Home = () => {
                         : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
                 }
             </div>
-        </>
+        </div>
     )
 }
 
