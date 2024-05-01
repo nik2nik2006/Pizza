2816f0ba41051f26bcb08d38de992b5967454aa2
nik2nik2006 <belnv@yandex.ru>
2024-05-01 22:42:40 +0300

    step 10. Pagination on page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,14 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import PizzaBlock from "../components/PizzaBlock";
+import Pagination from "../Pagination";
 
 
 const Home = ({searchValue}) => {
     const [items, setItems] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
     const [categoryId, setCategoryId] = React.useState(0);
+    const [currentPage, setCurrentPage] = React.useState(1);
     const [sortType, setSortType] = React.useState({
         name: 'популярности',
         sortProperty: 'rating'
@@ -21,7 +23,7 @@ const Home = ({searchValue}) => {
         const category = categoryId > 0 ? `category=${categoryId}` : '';
         const search = searchValue ? `&search=${searchValue}` : '';
         console.log(search, 'SEARCH')
-        fetch(`https://6603e6a62393662c31d00976.mockapi.io/items?${
+        fetch(`https://6603e6a62393662c31d00976.mockapi.io/items?page=${currentPage}&limit=4&${
             category}${
             search}&sortBy=${sortType.sortProperty}&order=${orderBy}`,
         )
@@ -31,7 +33,7 @@ const Home = ({searchValue}) => {
                 setIsLoading(false);
             });
         window.scrollTo(0, 0);
-    }, [categoryId, sortType, orderBy, searchValue]);
+    }, [categoryId, sortType, orderBy, searchValue, currentPage]);
 
     // фильтрация пицц без запроса на бекэнд
     // const pizzas = items.filter((obj) => {
@@ -56,6 +58,7 @@ const Home = ({searchValue}) => {
             <div className="content__items">
                 { isLoading ? skeletons : pizzas }
             </div>
+            <Pagination onChangePage={number => setCurrentPage(number)}/>
         </div>
     )
 }
